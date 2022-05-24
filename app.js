279a1f5c56d53fc6b17280506348653ecf9f934e
HossamMuhammedOmar <hossamomar9876@gmail.com>
2022-05-24 17:45:20 +0200

    finsih project

--- app.js
+++ app.js
@@ -4,19 +4,33 @@ const nextButton = document.getElementById("carousel-button-next");
 
 const imageItems = document.getElementsByClassName("carousel-item");
 
-let currentImage = 1;
+let currentImage = 0;
 
 prevButton.style.visibility = "hidden";
 
-nextButton.addEventListener("click", (e) => {
+nextButton.addEventListener("click", () => {
   currentImage += 1;
-  if (currentImage <= imageItems.length) {
-    imageItems[currentImage - 1].classList.add("carousel-item-visible");
-    imageItems[currentImage - 2].classList.remove("carousel-item-visible");
+  if (currentImage < imageItems.length) {
+    imageItems[currentImage].classList.add("carousel-item-visible");
+    imageItems[currentImage - 1].classList.remove("carousel-item-visible");
     prevButton.style.visibility = "visible";
   }
 
-  if (currentImage === imageItems.length) {
+  // CurrentImage = 2
+  if (currentImage === imageItems.length - 1) {
     nextButton.style.visibility = "hidden";
+  }
+});
+
+prevButton.addEventListener("click", () => {
+  if (currentImage !== 0) {
+    currentImage -= 1;
+    imageItems[currentImage + 1].classList.remove("carousel-item-visible");
+    imageItems[currentImage].classList.add("carousel-item-visible");
+  }
+
+  if (currentImage === 0) {
+    prevButton.style.visibility = "hidden";
+    nextButton.style.visibility = "visible";
   }
 });
